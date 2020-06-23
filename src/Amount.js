6f3ffce95e88f80a0346e9848a0e528b171757b5
musabgulfam <40719485+musabgulfam@users.noreply.github.com>
2020-06-23 23:52:13 +0500

    Updated Amount.js
    Removed some unused variables.

--- src/Amount.js
+++ src/Amount.js
@@ -2,10 +2,8 @@ import React, { useContext } from 'react'
 import Credit from './Credit'
 import a from './GlobalAmountContext'
 import debit from './GlobalContextDebit'
-import credit from './GlobalContextCredit'
 
 let Amount = () => {
-    let am=useContext(a);
     let c=React.useContext(credit);
     let d=React.useContext(debit);
     //let [amount, setAmount] = React.useState(0);
